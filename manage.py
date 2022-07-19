61054beddd448e00d27e3196e93fa92b037b38ae
Ali Haider Govani <alihaider12qw@hotmail.com>
2022-07-20 02:34:06 +0500

    prod test

--- manage.py
+++ manage.py
@@ -5,16 +5,16 @@ Use "python manage.py runserver" to start the development web server on localhos
 Use "python manage.py runserver --help" for additional runserver options.
 """
 
-from flask_migrate import MigrateCommand
-from flask_script import Manager
+# from flask_migrate import MigrateCommand
+# from flask_script import Manager
 
-from app import create_app
-from app.commands import InitDbCommand
+# from app import create_app
+# from app.commands import InitDbCommand
 
 # Setup Flask-Script with command line commands
-manager = Manager(create_app)
-manager.add_command('db', MigrateCommand)
-manager.add_command('init_db', InitDbCommand)
+# manager = Manager(create_app)
+# manager.add_command('db', MigrateCommand)
+# manager.add_command('init_db', InitDbCommand)
 
 # if __name__ == "__main__":
 #     # python manage.py                      # shows available commands
--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,5 @@
-from flask_migrate import MigrateCommand
-from flask_script import Manager
+from app import app
 
-from app import create_app
-from app.commands import InitDbCommand
 
 if __name__ == "__main__":
-    manager = Manager(create_app)
-    manager.add_command('db', MigrateCommand)
-    manager.add_command('init_db', InitDbCommand)
-    manager.run()
+    app.run()
